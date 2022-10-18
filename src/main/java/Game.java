33ec2d76ab1e9e70d9927b69b41a345c0b7deb3a
Luís Alves <luiscnalves@gmail.com>
2022-10-18 01:31:08 +0100

    Reading KeyStrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,19 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.security.Key;
 
 public class Game {
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20); // tamanho do terminal
@@ -24,16 +30,34 @@ public class Game {
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() {
-        try {
-            draw();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true){
+            try {
+                draw();
+                KeyStroke key = screen.readInput();
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                    screen.close();
+                    break;
+                }
+                else if (key.getKeyType() == KeyType.EOF) { // se fechar a janela
+                    break;
+                }
+                processKey(key);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private Screen screen;
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp -> y -= 1; // se fizer desta forma não preciso de usar break
+            case ArrowDown -> y += 1;
+            case ArrowLeft -> x -= 1;
+            case ArrowRight -> x += 1;
+        }
+    }
 }
